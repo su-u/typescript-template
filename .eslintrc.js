36e5cbf927e51288fef74bfcdbf6de255740da48
su- <aaaiiiuuu35@gmail.com>
2020-05-08 00:00:55 +0900

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,7 +35,7 @@ module.exports = {
     'no-extra-semi': 'error',
     'no-unexpected-multiline': 'error',
     'no-unreachable': 'error',
-    indent: [2, 2],
+    indent: ['error', 2, { SwitchCase: 1 }],
 
     'no-extra-bind': 'warn',
     'no-fallthrough': 'warn',
